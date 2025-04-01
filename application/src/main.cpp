53ea531a1416a411ef823957c972674d0e917387
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-04-01 22:27:59 +0100

    Initial commit (CMake + GLFW setup)

---
+++ application/src/main.cpp
@@ -0,0 +1,19 @@
+#include "HelperGLFW.h"
+
+int main()
+{
+	uint32_t width = 1280;
+	uint32_t height = 800;
+	
+	GLFWwindow* window = initWindow("GLFW Example", width, height);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	
+	return 0;
+}
