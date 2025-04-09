e42bb6576022cf42f137945012a4983bd7bb20c4
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-04-09 14:33:57 +0100

    Create a GLFW window

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <shared/HelpersGLFW.h>
 
-int main() {
-	printf("Hello world.\n");
+int main()
+{
+	uint32_t width = 1280;
+	uint32_t height = 800;
+	
+	GLFWwindow* window = initWindow("Rock", width, height);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
