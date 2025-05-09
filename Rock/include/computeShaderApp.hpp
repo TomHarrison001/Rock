d4e7840f6ea89118eb59df7518ac07fab7c0330f
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-05-09 11:55:53 +0100

    Create compute shader app class

---
+++ Rock/include/computeShaderApp.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <random>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "core/application.hpp"
+#include "rendering/pipeline.hpp"
+
+struct UniformBufferObject {
+	float u_deltaTime = 1.f;
+};
+
+class ComputeShaderApp : public Application
+{
+private:
+	void initApplication() override;
+	void mainLoop() override;
+	void cleanup() override;
+	void createDescriptorPool();
+	void createGraphicsDescriptorSetLayout();
+	void createComputeDescriptorSetLayout();
+	void createGraphicsDescriptorSets();
+	void createComputeDescriptorSets();
+	void createRenderPipeline();
+	void createComputePipeline();
+	void createShaderStorageBuffers();
+	void createUniformBuffers();
+	void updateUniformBuffer(uint32_t currentImage);
+public:
+	void run() override;
+	void drawFrame() override;
+private:
+	Pipeline* m_graphicsPipeline;
+	Pipeline* m_computePipeline;
+	const uint32_t m_particleCount = 8192;
+
+	std::vector<VkBuffer> m_shaderStorageBuffers;
+	std::vector<VkDeviceMemory> m_shaderStorageBuffersMemory;
+
+	VkImageView m_textureImageView;
+	VkSampler m_textureSampler;
+	std::vector<VkBuffer> m_uniformBuffers;
+	std::vector<VkDeviceMemory> m_uniformBuffersMemory;
+	std::vector<void*> m_uniformBuffersMapped;
+
+	float m_lastFrameTime = 0.f;
+	double m_lastTime = 0.f;
+};
