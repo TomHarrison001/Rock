1252959531bafd7d19077cd6152a796444589443
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-05-14 19:58:17 +0100

    Organise shaders by application

--- compileShaders.bat
+++ compileShaders.bat
@@ -1,4 +1,6 @@
-C:/VulkanSDK/1.4.309.0/Bin/glslc.exe ./Rock/res/shaders/main.vert -o ./Rock/res/shaders/vert.spv
-C:/VulkanSDK/1.4.309.0/Bin/glslc.exe ./Rock/res/shaders/main.frag -o ./Rock/res/shaders/frag.spv
-C:/VulkanSDK/1.4.309.0/Bin/glslc.exe ./Rock/res/shaders/main.comp -o ./Rock/res/shaders/comp.spv
+C:/VulkanSDK/1.4.309.0/Bin/glslc.exe ./Rock/res/shaders/modelApp/main.vert -o ./Rock/res/shaders/modelApp/vert.spv
+C:/VulkanSDK/1.4.309.0/Bin/glslc.exe ./Rock/res/shaders/modelApp/main.frag -o ./Rock/res/shaders/modelApp/frag.spv
+C:/VulkanSDK/1.4.309.0/Bin/glslc.exe ./Rock/res/shaders/computeApp/main.vert -o ./Rock/res/shaders/computeApp/vert.spv
+C:/VulkanSDK/1.4.309.0/Bin/glslc.exe ./Rock/res/shaders/computeApp/main.frag -o ./Rock/res/shaders/computeApp/frag.spv
+C:/VulkanSDK/1.4.309.0/Bin/glslc.exe ./Rock/res/shaders/computeApp/main.comp -o ./Rock/res/shaders/computeApp/comp.spv
 pause
